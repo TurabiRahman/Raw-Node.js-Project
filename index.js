a778f34218dadeb1f55b00919819679f78f598a3
TurabiRahman <rahman.turabi.10@gmail.com>
2026-04-18 07:14:18 +0600

    feat: modules

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-const people = require('./people');
-console.log(people);
+const path = require('path');
 
+const myPath = 'C:/Users/USERAS/Downloads/Raw-Node.js-Project/index.js';
+
+console.log(path.basename(myPath)); // C:/Users/USERAS/Downloads/Raw-Node.js-Project
+console.log(path.parse(myPath)); // C:/Users/USERAS/Downloads/Raw-Node.js-Project
+
+const os = require('os');
+
+// console.log(os.platform());
+// console.log(os.cpus());
+
+const fs = require('fs');
+
+fs.writeFileSync('modules.txt', 'The modules i learnt in Node.js are: os, path, fs');
+fs.readFile('modules.txt', (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data.toString());
+    }
+});
